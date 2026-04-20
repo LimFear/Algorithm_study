01def7013e103eaf22c7d8a0757b89bb0bb13b1e
LimFear <dlawjdghks@gmail.com>
2026-04-20 14:27:21 +0900

    Title: 괄호 쌍 만들어주기 3, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260420/괄호 쌍 만들어주기 3/pair-parentheses-3.py
@@ -0,0 +1,18 @@
+A = input()
+
+# Please write your code here.
+A = list(map(str, A))
+print(A)
+
+cnt = 0
+
+for i in range(len(A) - 1):
+    if A[i] == '(':
+        for j in range(i + 1, len(A)):
+            if A[j] == ')':
+                cnt += 1
+
+    else:
+        continue
+
+print(cnt)
